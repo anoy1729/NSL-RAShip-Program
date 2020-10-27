f1bded334286bdb7dc3e1c8bb6df8f9db12c0c01
anoy1729 <51030142+anoy1729@users.noreply.github.com>
2020-10-27 18:21:38 +0600

    Create Mean, Var, and Std.py

---
+++ python-basic/Numpy/Mean, Var, and Std.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+inp = list(map(int, input().split()))
+arr = []
+
+for i in range(inp[0]):
+    inp2 = list(map(int, input().split()))
+    arr.append(inp2)
+    
+nparr = np.array(arr)
+np.set_printoptions(legacy='1.13')
+print (np.mean(nparr, axis = 1)) 
+print (np.var(nparr, axis = 0)) 
+print (np.std(nparr, axis = None)) 
+
